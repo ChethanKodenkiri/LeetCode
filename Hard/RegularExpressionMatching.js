84a8d0cf3b00bd7eb329ef644ec164506bacbf02
ChethanKodenkiri <chethankodenkiri@gmail.com>
2024-04-05 01:17:44 +0530

    + Add : Hard Added Regular Expression matching A <signed by:chethan_kodenkiri>

--- Hard/RegularExpressionMatching.js
+++ Hard/RegularExpressionMatching.js
@@ -44,5 +44,29 @@ It is guaranteed for each appearance of the character '*', there will be a previ
  * @return {boolean}
  */
 var isMatch = function(s, p) {
+  function dfs(i,j){
+    if(i>= s.length && j>= p.length)
+        return true;
+
+    if(j>=p.length)
+        return false;
+
+
+    const match = (i<= (s.length)) && (s[i] == p[j])||(p[j]=='.')
+
     
+    if(j+1 <p.length && p[j+1]=='*'){
+        return dfs(i,j+2) || (match && dfs(i+1,j));
+    }
+
+    if(match){ 
+       return dfs(i+1,j+1)
+    }
+     return false;
+
+  }
+  return dfs(0,0)
+
 };
+
+console.log(isMatch('aa','aa'))
