89cbc0d437c9125d1a7b5986486249fe33db6e34
ChethanKodenkiri <chethankodenkiri@gmail.com>
2024-04-05 22:36:04 +0530

    + Add : Medium Added Container with Most water Q  <signed by:chethan_kodenkiri>

--- Hard/RegularExpressionMatching.js
+++ Hard/RegularExpressionMatching.js
@@ -43,6 +43,8 @@ It is guaranteed for each appearance of the character '*', there will be a previ
  * @param {string} p
  * @return {boolean}
  */
+
+/*
 var isMatch = function(s, p) {
   function dfs(i,j){
     if(i>= s.length && j>= p.length)
@@ -70,3 +72,52 @@ var isMatch = function(s, p) {
 };
 
 console.log(isMatch('aa','aa'))
+
+*/
+
+
+
+var isMatch = function(s, p) {
+    // recursion basic case
+    if (!p || !p.length) return !Boolean(s.length);
+    
+    // first char match condition
+    const first_match = s.length && (p[0] === s[0] || p[0] === '.');
+	
+    if (p.length > 1 && p[1] === '*') {
+		// If the second char is wildcard, then result to two cases:
+		
+		// 1.the first char doesn't match, so the second wildcard can repeat the first char of p 0 times
+		// then continue to match the rest part of s
+		const condition1 = isMatch(s, p.substring(2));
+		
+		// Example
+		// s: aab
+		// p: c*a*b*
+		
+		// Explaination: c* can repeat c 0 times, then continue to match s with the rest part of p
+		
+		// 2.The first char does matches, so the second wildcard can repeat the first char any times
+		// then continue to match the rest part of s
+		const condition2 = first_match && isMatch(s.substring(1), p);
+		
+		// Example 1
+		// s: aa
+		// p: a*
+		
+		// Explaination: a* can repeat a two times then becomes to aa which matches aa
+		
+		// Example 2
+		// s: ab
+		// p: .*
+		
+		//Explaination: .* can repeat . two times then becomes to .. which matches ab
+		
+		return condition1 || condition2;
+    } else {
+        // The normal case is to match the first char then recursively match the rest chars
+        return first_match && isMatch(s.substring(1), p.substring(1));
+    }
+};
+
+console.log(isMatch('aa','a*'))
