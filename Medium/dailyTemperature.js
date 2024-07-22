f1b1812959b3d0a323aaa70655b234ad4a836693
ChethanKodenkiri <chethankodenkiri@gmail.com>
2024-07-22 22:01:24 +0530

    +Add: Added Car Fleet Q&A

--- Medium/dailyTemperature.js
+++ Medium/dailyTemperature.js
@@ -35,7 +35,7 @@ var dailyTemperatures = function(temperatures) {
     let result = Array(temperatures.length).fill(0)
     let stack =[]
     for(let i=0;i<temperatures.length;i++){
-        while(stack.length && temperatures[i]> stack[stack.length-1][0]){
+        while(stack.length0 && temperatures[i]> stack[stack.length-1][0]){
             let [stackTemp, stackIndex]=stack.pop()
             result[stackIndex]=i-stackIndex
         }
