52da40f65628f8efd76060a641280851a764453a
ChethanKodenkiri <chethankodenkiri@gmail.com>
2024-07-27 23:19:43 +0530

    ^Updtae: Optimized Search 2D matrix

--- Medium/Search2DMatrix.js
+++ Medium/Search2DMatrix.js
@@ -54,4 +54,35 @@ var searchMatrix = function(matrix, target) {
     return false
 };
 
-console.log(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]],13))
+
+function searchMatrixs(matrix,target){
+    let [ROW,COL]=[matrix.length,matrix[0].length]
+ 
+    let[top,bot]=[0,ROW-1]
+
+    while(top<=bot){
+        let row = Math.floor((top+bot)/2)
+       
+        if(matrix[row][COL-1] <target){  
+            top=row+1
+        }else if(matrix[row][0]>target){   
+            bot=row-1
+        }else{break}
+    }
+    if(!(top<=bot)){return false}
+
+    let row = Math.floor((top+bot)/2)
+    console.log(row)
+    let[start,end]=[0,COL-1]
+    while(start<=end){
+       let middle = Math.floor((start+end)/2)
+       if(matrix[row][middle]>target){end=middle-1}
+       else if(matrix[row][middle]<target){start=middle+1}
+       else if(matrix[row][middle]===target){return true} 
+    }
+    return false
+}
+
+
+
+console.log(searchMatrixs([[1,3,5,7],[10,11,16,20],[23,30,34,60],[100,300,340,600]],3))
